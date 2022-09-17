71f9de5aa910a25763f3cc661cd0599b022296f1
Tony Mottaz <mail@tonymottaz.com>
2022-09-17 08:16:17 -0500

    initial site using 11ty

---
+++ .eleventy.js
@@ -0,0 +1,69 @@
+const { DateTime } = require("luxon");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const prettier = require("prettier");
+const postcss = require("postcss");
+const autoprefixer = require("autoprefixer");
+const cssnesting = require("postcss-nesting");
+const csso = require("postcss-csso");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(src, {
+    widths: [300, 600],
+    formats: ["avif", "jpeg"],
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async",
+  };
+
+  // You bet we throw an error on missing alt in `imageAttributes` (alt="" works okay)
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy(".nojekyll");
+  eleventyConfig.addPassthroughCopy("font");
+  eleventyConfig.addPassthroughCopy("img");
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+
+  eleventyConfig.addFilter("postcss", function (code) {
+    return postcss([autoprefixer, cssnesting(), csso]).process(code).css;
+  });
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLLL yyyy"
+    );
+  });
+
+  eleventyConfig.addPlugin(syntaxHighlight, {
+    preAttributes: {
+      "data-language": function ({ language }) {
+        return language;
+      },
+    },
+  });
+
+  eleventyConfig.addPlugin(pluginRss);
+
+  eleventyConfig.addTransform("prettier", function (content) {
+    if (this.outputPath?.endsWith(".html")) {
+      return prettier.format(content, { filepath: this.outputPath });
+    }
+    return content;
+  });
+
+  return { dir: { input: "src" } };
+};
