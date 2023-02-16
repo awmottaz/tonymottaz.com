6e116b03dff7d52174ac0746fac7044a87f905fc
Tony Mottaz <mail@tonymottaz.com>
2023-02-15 21:57:06 -0600

    config: add support for date formatting

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,4 @@
+const { DateTime } = require("luxon");
 const pluginBundle = require("@11ty/eleventy-plugin-bundle");
 
 module.exports = function (eleventyConfig) {
@@ -6,6 +7,20 @@ module.exports = function (eleventyConfig) {
 	});
 
 	eleventyConfig.addPlugin(pluginBundle);
+
+	eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+		// https://moment.github.io/luxon/#/formatting?id=table-of-tokens
+		return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+			"yyyy-LL-dd"
+		);
+	});
+
+	eleventyConfig.addFilter("readableDate", (dateObj) => {
+		return new Intl.DateTimeFormat("en-US", { dateStyle: "long" }).format(
+			dateObj
+		);
+	});
 
 	return {
 		templateFormats: ["html", "md", "njk"],
