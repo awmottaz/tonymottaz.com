9151048c366471e3cbc5c5e9ecde9a7bf42a0ed5
Tony Mottaz <mail@tonymottaz.com>
2023-07-17 16:36:02 -0500

    convert feed to liquid

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,15 @@
 const { DateTime } = require("luxon");
 const prettier = require("prettier");
 const path = require("node:path");
+const posthtml = require("posthtml");
+const urls = require("posthtml-urls");
 
 const pluginBundle = require("@11ty/eleventy-plugin-bundle");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 const pluginMarkdown = require("./11ty-plugins/markdown.js");
 const pluginSass = require("./11ty-plugins/sass.js");
+
+const absoluteUrl = (rel, base) => new URL(rel, base).toString();
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = function (eleventyConfig) {
@@ -18,7 +21,39 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginBundle);
   eleventyConfig.addPlugin(pluginMarkdown);
   eleventyConfig.addPlugin(pluginSass);
-  eleventyConfig.addPlugin(pluginRss);
+
+  eleventyConfig.addFilter("absoluteUrl", absoluteUrl);
+
+  eleventyConfig.addAsyncFilter(
+    "htmlToAbsoluteUrls",
+    async (htmlContent, base) => {
+      const plugin = urls({ eachURL: (url) => absoluteUrl(url.trim(), base) });
+      const { html } = await posthtml().use(plugin).process(htmlContent);
+      return html;
+    },
+  );
+
+  eleventyConfig.addFilter("getNewestCollectionItemDate", (collection) => {
+    if (!collection?.length) {
+      return new Date();
+    }
+
+    return new Date(
+      Math.max(
+        ...collection.map((item) => {
+          return item.date;
+        }),
+      ),
+    );
+  });
+
+  // Atom uses RFC 3339 dates
+  // https://tools.ietf.org/html/rfc3339#section-5.8
+  eleventyConfig.addFilter("dateToRfc3339", (date) => {
+    const isoDate = date.toISOString();
+    const [isoDateWithoutMS] = isoDate.split(".");
+    return `${isoDateWithoutMS}Z`;
+  });
 
   eleventyConfig.addFilter("htmlDateString", (dateObj) => {
     // dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
